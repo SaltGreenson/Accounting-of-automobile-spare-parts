270599eaeaae126506de78afb47a46bf81a0daa1
Salt Greenson <vladyuskovich@gmail.com>
2022-06-03 22:35:31 +0300

    was added orders

--- CAR_SPARE_PARTS/Models/Order/Order.cs
+++ CAR_SPARE_PARTS/Models/Order/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,14 @@ namespace CAR_SPARE_PARTS.Models.Order
 {
     public class Order
     {
-        [Key] public int UserID { get; set; }
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int ID { get; set; }
+        [Required] public int UserID { get; set; }
         [Required][MaxLength(25)] public string LastName {get; set;}
         [Required][MaxLength(25)] public string Name { get; set; }
         [Required][MaxLength(25)] public string MiddleName { get; set; }
         [Required][MaxLength(25)] public string Address { get; set; }
 
-        [Required] public DateTime Date { get; set; }
+        [Required] public string Date { get; set; }
         [Required] public double PriceOfOrder { get; set; }
     }
 }
